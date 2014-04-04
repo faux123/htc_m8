e55da98bd55d4a81a0284f60dd1f761c1b48a89c
flar2 <asegaert@gmail.com>
2014-04-04 12:13:09 -0400

    Fastcharge for HTC One m8

--- drivers/power/qpnp-charger.c
+++ drivers/power/qpnp-charger.c
@@ -40,6 +40,11 @@
 #ifdef CONFIG_HTC_BATT_8960
 #include "mach/htc_battery_cell.h"
 #endif
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #ifdef pr_debug
 #undef pr_debug
 #endif
@@ -2802,7 +2807,10 @@ int pm8941_set_pwrsrc_and_charger_enable(enum htc_power_source_type src,
 	case HTC_PWR_SOURCE_TYPE_DETECTING:
 	case HTC_PWR_SOURCE_TYPE_UNKNOWN_USB:
 	case HTC_PWR_SOURCE_TYPE_USB:
-		mA = USB_MA_500;
+		if (force_fast_charge)
+			mA = USB_MA_1100;
+		else
+			mA = USB_MA_500;
 		break;
 	case HTC_PWR_SOURCE_TYPE_AC:
 	case HTC_PWR_SOURCE_TYPE_9VAC:
